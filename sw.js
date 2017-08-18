d0649e5f6d33c5e2bec8da9822965642af954187
nobuhiko1107 <30885707+nobuhiko1107@users.noreply.github.com>
2017-08-18 14:44:57 +0900

    new2

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-
-self.addEventListener('fetch', event => {
-  event.respondWith(new Response('Hello World!'));
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/test/.html',
+        '/sw.js',
+        '/test.png'
+      ]);
+    })
+  );
 });
