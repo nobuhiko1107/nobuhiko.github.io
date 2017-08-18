4508ea08cbc847c3494515f6fa628cd563d20463
nobuhiko1107 <30885707+nobuhiko1107@users.noreply.github.com>
2017-08-18 15:44:58 +0900

    new2

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
+<<<<<<< HEAD
         '/test.html',
         '/main.css',
         '/sw.js',
         '/test.jpg',
         
+=======
+      return cache.addAll([
+        '/test/.html',
+        '/sw.js',
+        '/test.png'
+>>>>>>> new2
       ]);
     })
   );
